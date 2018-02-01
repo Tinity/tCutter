d641f57f4c01ba9ea7d29ff0d5f919f69bdf164f
Tinity <34384753+Tinity@users.noreply.github.com>
2018-02-01 20:58:10 +0200

    Oak update
    Oak update

--- Tinity/Nodes/CutNode.java
+++ Tinity/Nodes/CutNode.java
@@ -3,11 +3,14 @@ package Tinity.Nodes;
 import Tinity.MyFirstNodeScript;
 import Tinity.Node;
 import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.skills.Skill;
 
 
 public class CutNode extends Node {
     private static final Area TREE_AREA = new Area(3116, 3444, 3141, 3419);
+    private static final Area OAK_AREA = new Area(3123,3443,3136,3429);
     private static final int TREE = 1278;
+    private static final int OAK = 1751;
 
     public CutNode(MyFirstNodeScript main) {
         super(main);
@@ -20,8 +23,15 @@ public class CutNode extends Node {
 
     @Override
     public int execute() {
-        c.log("Cutting...");
-        c.getGameObjects().closest(TREE).interact("Chop down");
+
+        if (c.getSkills().getRealLevel(Skill.WOODCUTTING) >= 15) {
+            c.log("Cutting Oak...");
+            c.getGameObjects().closest(OAK).interact("Chop down");
+        } else {
+            c.log("Cutting Tree...");
+            c.getGameObjects().closest(TREE).interact("Chop down");
+        }
+
 
 
         return 1000;
